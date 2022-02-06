ef6a8be2c3e463682a0adc81b9abaca9577f7e6c
Jhessy <jhessyismael@gmail.com>
2022-02-06 16:25:26 +0100

    Création des dossiers common (pages qu'on retouver partout ) et pages (pages fix)

--- Frontend/atypikhouse-front/src/app/app.module.ts
+++ Frontend/atypikhouse-front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './common/main-menu//main-menu.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { MainFooterComponent } from './common/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    HomepageComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
