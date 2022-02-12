8c2eb1eaa217a166dbd43762bca539e6a4730609
Alexandre Wotawa <awotawa@gmail.com>
2022-02-12 15:57:30 +0100

    created the Property entity using 'php bin/console make:entity' command & setting the fields for it with their type

---
+++ Backend/src/Entity/Property.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PropertyRepository::class)]
+class Property
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'float')]
+    private $rate;
+
+    #[ORM\Column(type: 'array', nullable: true)]
+    private $photos = [];
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $description;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $address;
+
+    #[ORM\Column(type: 'time', nullable: true)]
+    private $check_in;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getPhotos(): ?array
+    {
+        return $this->photos;
+    }
+
+    public function setPhotos(?array $photos): self
+    {
+        $this->photos = $photos;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getCheckIn(): ?\DateTimeInterface
+    {
+        return $this->check_in;
+    }
+
+    public function setCheckIn(?\DateTimeInterface $check_in): self
+    {
+        $this->check_in = $check_in;
+
+        return $this;
+    }
+}
