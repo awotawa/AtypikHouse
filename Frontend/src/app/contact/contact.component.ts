bcb9c838a97f2d62d490572596d4b41205301e9a
Alex <alexandre.huynh98@gmail.com>
2022-02-12 20:48:47 +0100

    Formulaire contact avancé + récupération valeur - 12/02/2022

--- Frontend/src/app/contact/contact.component.ts
+++ Frontend/src/app/contact/contact.component.ts
@@ -7,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  name!: string;
+  email: string | undefined;
+  message: string | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
     // create api call
     // create the form based on api fields
 
+  }
+
+  submitForm(){
+    const message =`Mon name est ${this.name}.
+    Mon email est ${this.email}.
+    Mon message est : ${this.message}`;
+
+    //Récupère les valeurs de tous les champs du formulaire
+    alert(message);
   }
 
 }
