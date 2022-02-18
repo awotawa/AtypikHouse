977e05bd689d16b50abeda718d285d143f0c186a
Alexandre Wotawa <awotawa@gmail.com>
2022-02-18 16:32:25 +0100

    created the Lodging Entity

---
+++ Backend/src/Entity/Lodging.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LodgingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LodgingRepository::class)]
+class Lodging
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'float')]
+    private $rate;
+
+    #[ORM\Column(type: 'array')]
+    private $photos = [];
+
+    #[ORM\Column(type: 'text')]
+    private $lodging_description;
+
+    #[ORM\Column(type: 'text')]
+    private $adress;
+
+    #[ORM\Column(type: 'time')]
+    private $check_in_time;
+
+    #[ORM\Column(type: 'datetime')]
+    private $created_at;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRate(): ?float
+    {
+        return $this->rate;
+    }
+
+    public function setRate(float $rate): self
+    {
+        $this->rate = $rate;
+
+        return $this;
+    }
+
+    public function getPhotos(): ?array
+    {
+        return $this->photos;
+    }
+
+    public function setPhotos(array $photos): self
+    {
+        $this->photos = $photos;
+
+        return $this;
+    }
+
+    public function getLodgingDescription(): ?string
+    {
+        return $this->lodging_description;
+    }
+
+    public function setLodgingDescription(string $lodging_description): self
+    {
+        $this->lodging_description = $lodging_description;
+
+        return $this;
+    }
+
+    public function getAdress(): ?string
+    {
+        return $this->adress;
+    }
+
+    public function setAdress(string $adress): self
+    {
+        $this->adress = $adress;
+
+        return $this;
+    }
+
+    public function getCheckInTime(): ?\DateTimeInterface
+    {
+        return $this->check_in_time;
+    }
+
+    public function setCheckInTime(\DateTimeInterface $check_in_time): self
+    {
+        $this->check_in_time = $check_in_time;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+}
