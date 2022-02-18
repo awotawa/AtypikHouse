904f64ce5474e67f980b46ff567286c51a99da45
Alexandre Wotawa <awotawa@gmail.com>
2022-02-18 18:55:54 +0100

    added in the updated_at property for the Lodging Entity

--- Backend/src/Entity/Lodging.php
+++ Backend/src/Entity/Lodging.php
@@ -31,6 +31,9 @@ class Lodging
     #[ORM\Column(type: 'datetime')]
     private $created_at;
 
+    #[ORM\Column(type: 'datetime')]
+    private $updated_at;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -104,6 +107,18 @@ class Lodging
     public function setCreatedAt(\DateTimeInterface $created_at): self
     {
         $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
 
         return $this;
     }
