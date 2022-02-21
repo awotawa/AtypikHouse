c4575f910ac993abea7977f9bc28aedfc0d514e2
Alexandre Wotawa <awotawa@gmail.com>
2022-02-21 07:58:37 +0100

    commented out the non-existent fields in the user form for the moment

--- Backend/tests/Entity/UserTest.php
+++ Backend/tests/Entity/UserTest.php
@@ -20,9 +20,9 @@ class ArticleTest extends KernelTestCase
       ->setLastName("Smith")
       ->setPhoto("john.smith@yopmail.com")
       ->setEmail("john.smith@yopmail.com")
-      ->setCreatedAt(new \DateTime('2005-08-15T15:52:01+00:00'))
-      ->setCreatedAt(new \DateTime('2005-08-15T15:52:01+00:00'))
-      ->setIsVerified(true)
+      // ->setCreatedAt(new \DateTime('2005-08-15T15:52:01+00:00'))
+      // ->setCreatedAt(new \DateTime('2005-08-15T15:52:01+00:00'))
+      // ->setIsVerified(true)
       ->setPassword("azertyuiop");
 
   }
