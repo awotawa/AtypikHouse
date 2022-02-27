67000585ab95d439863b8c2afa9a000fca0704b7
Alexandre Wotawa <awotawa@gmail.com>
2022-02-27 17:14:41 +0100

    files created after running the 'php bin/phpunit' command

--- Backend/tests/Entity/UserTest.php
+++ Backend/tests/Entity/UserTest.php
@@ -7,7 +7,7 @@ use App\Entity\User;
 use App\Tests\Entity\tools\AssertEntityTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class ArticleTest extends KernelTestCase
+class UserTest extends KernelTestCase
 {
   use AssertEntityTrait;
 
@@ -20,10 +20,10 @@ class ArticleTest extends KernelTestCase
       ->setLastName("Smith")
       ->setPhoto("https://randomuser.me/api/portraits/men/66.jpg")
       ->setEmail("john.smith@yopmail.com")
+      ->setPassword("azertyuiop");
       // ->setCreatedAt(new \DateTime('2005-08-15T15:52:01+00:00'))
       // ->setCreatedAt(new \DateTime('2005-08-15T15:52:01+00:00'))
       // ->setIsVerified(true)
-      ->setPassword("azertyuiop");
 
   }
 
@@ -31,4 +31,36 @@ class ArticleTest extends KernelTestCase
   {
     $this->assertHasErrors($this->user, 0);
   }
+
+  //FIRST_NAME TESTING
+  // Blank first_name
+  public function testFirstNameBlankUser(): void
+  {
+    $this->assertHasErrors($this->user->setFirstName(""), 2, 'first_name', 'This value should not be blank.');
+    $this->assertHasErrors($this->user->setFirstName(""), 2, 'first_name', 'Your first name must be at least 2 characters long');
+  }
+
+  // Wrong type first_name
+  // Too short first_name
+  // Too long first_name
+  // Invalid characters first_name
+
+  //LAST_NAME TESTING
+  // Blank last_name
+  // Wrong type last_name
+  // Too short last_name
+  // Too long last_name
+  // Invalid characters last_name
+
+  //EMAIL TESTING
+  // Blank email
+  // Wrong pattern email
+
+  //PASSWORD TESTING
+  // Blank password
+  // Too short password
+
+  //PHOTO TESTING
+  // No photo
+  // Wrong format photo
 }
