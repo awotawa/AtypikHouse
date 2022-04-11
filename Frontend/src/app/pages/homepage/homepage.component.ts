85162f31a56df7a3833a794bb46f6d632b8e350b
aruseus98 <aruseus98@gmail.com>
2022-04-11 15:53:50 +0200

    Ajout carousel sur la page d'accueil V1.0

--- Frontend/src/app/pages/homepage/homepage.component.ts
+++ Frontend/src/app/pages/homepage/homepage.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit(): void {
+    this.Repeat();
   }
 
+  public startIndex = 1;
+  
+  Repeat() {
+    setTimeout(() => {
+      this.__FunctionSlide();
+      this.Repeat();
+    }, 8000);
+  }
+
+  __FunctionSlide() {
+    const slides = Array.from(document.getElementsByClassName('mySlides'));
+    if (slides === []) {
+      this.Repeat();
+    }
+    for (const x of slides) {
+      const y = x as HTMLElement;
+      y.style.display = 'none';
+    }
+    if (this.startIndex > slides.length - 1) {
+      this.startIndex = 0;
+      const slide = slides[this.startIndex] as HTMLElement;
+      slide.style.display = 'block';
+      this.startIndex++;
+    } else {
+
+      const slide = slides[this.startIndex] as HTMLElement;
+      slide.style.display = 'block';
+      this.startIndex++;
+    }
+  }
+  
 }
