f1628fad79148a4bfd2ee7fffe2bfa198be85256
Alexandre Wotawa <awotawa@gmail.com>
2022-04-23 18:27:43 +0200

    added a way to visualize the error messages

--- Backend/src/Controller/ApiPostController.php
+++ Backend/src/Controller/ApiPostController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Lodging;
+use Doctrine\ORM\EntityManager;
 use App\Repository\LodgingRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Exception\NotEncodableValueException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ApiPostController extends AbstractController
 {
-    #[Route('/api/lodging', name: 'app_api_post_index', methods: ['GET'])]
+    #[Route('/api/manual/lodging', name: 'app_api_post_index', methods: ['GET'])]
     public function index(LodgingRepository $lodgingRepository, SerializerInterface $serializer): Response
     {
         $lodgings = $lodgingRepository->findAll();
@@ -37,5 +43,37 @@ class ApiPostController extends AbstractController
         // $response = this->json($lodgings, 200, [], []); #json method inherited from AbstractController
 
         return $response;
+    }
+
+    #[Route('/api/manual/lodging', name: 'app_api_post_new', methods: ['POST'])]
+    public function new(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, ValidatorInterface $validator)
+    {
+      $jsonRecu = $request->getContent();
+
+      try {
+        $lodging = $serializer->deserialize($jsonRecu, Lodging::class, 'json');
+
+        $errors = $validator->validate($lodging);
+
+        if(count($errors) > 0){
+          return $this->json($errors, 400);
+        }
+
+        $em->persist($lodging);
+        $em->flush();
+
+        // dd($lodging);
+
+        return $this->json($lodging, 201, []);
+
+      } catch (NotEncodableValueException $e) {
+
+        return $this->json([
+          'status' => 400,
+          'message' => $e->getMessage()
+        ], 400);
+
+      }
+
     }
 }
