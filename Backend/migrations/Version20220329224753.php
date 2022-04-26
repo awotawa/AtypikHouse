24f9c0a2e50fb6625547f03f59ee81017c78ea6f
Alexandre Wotawa <awotawa@gmail.com>
2022-04-26 08:39:54 +0200

    commented out line in the up that would prohibit me from running the migrations

--- Backend/migrations/Version20220329224753.php
+++ Backend/migrations/Version20220329224753.php
@@ -20,7 +20,7 @@ final class Version20220329224753 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, CHANGE roles roles JSON NOT NULL, CHANGE photo photo VARCHAR(255) DEFAULT NULL');
+        //$this->addSql('ALTER TABLE user ADD created_at DATETIME NOT NULL, ADD updated_at DATETIME NOT NULL, CHANGE roles roles JSON NOT NULL, CHANGE photo photo VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
