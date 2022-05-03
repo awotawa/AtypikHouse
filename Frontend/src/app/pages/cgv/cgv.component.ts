1aca5475129c9a97525aa57a2db0e9d5bf6fea3a
aruseus98 <aruseus98@gmail.com>
2022-05-03 23:29:45 +0200

    Retirer indexation certaines pages

--- Frontend/src/app/pages/cgv/cgv.component.ts
+++ Frontend/src/app/pages/cgv/cgv.component.ts
@@ -21,8 +21,8 @@ export class CGVComponent implements OnInit {
     addTag() {
       this.metaService.addTag({ httpEquiv: 'Content-Type', content: 'text/html' }); // Indique aux agents et serveurs de prendre le contenu de cette page en tant que HTML
       this.metaService.addTag({ name: 'description', content: "Conditions générales de vente d'Atypik House, site de location de logement insolite en France" }); // Meta description de la page
-      this.metaService.addTag({ property: 'og-type', content: "Site web"}); /* Indique le type de l'objet
-      /*this.metaService.addTag({ name: 'robots', content: 'index,follow' }); */ // Permet au robot d'indexer la page
+      this.metaService.addTag({ property: 'og-type', content: "Site web"}); // Indique le type de l'objet
+      this.metaService.addTag({ name: 'robots', content: 'noindex, nofollow' });  // Permet au robot d'indexer la page
       /* this.metaService.addTag({ property: 'og:title', content: 'Content Title for social media' }); */ // Titre pour réseau social
       /*this.metaService.addTag({ name: 'keywords', content: 'TypeScript, Angular' });*/ //Add keyword
       /* this.metaService.addTag({ property: 'og:title', content: "My Text" }) */ // Titre pour l'encadré dans les recherches
