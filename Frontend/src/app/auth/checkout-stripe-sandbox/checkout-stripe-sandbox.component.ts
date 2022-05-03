571dbf6be0aad6c3beee00ded7c223ebb4654e96
aruseus98 <aruseus98@gmail.com>
2022-05-03 23:36:26 +0200

    Rajout balise SEO pour page de paiement

--- Frontend/src/app/auth/checkout-stripe-sandbox/checkout-stripe-sandbox.component.ts
+++ Frontend/src/app/auth/checkout-stripe-sandbox/checkout-stripe-sandbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-checkout-stripe-sandbox',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutStripeSandboxComponent implements OnInit {
 
+  title ='Paiement | Atypik House | Location de logement | France';
+
   paymentHandler:any = null;
 
-  constructor() { }
+  constructor(private metaService:Meta, private titleService: Title) { 
+    this.addTag();
+    this.titleService.setTitle(this.title);
+  }
+
+
+  addTag() {
+    /*this.metaService.addTag({ charset: 'UTF-8' }); // Set en UTF 8
+    this.metaService.addTag({ name: 'viewport', content: 'width=device-width, initial-scale=1' }); */ // Donne comme instruction au browser comment controler la dimension et l'échelle de la page
+    this.metaService.addTag({ httpEquiv: 'Content-Type', content: 'text/html' }); // Indique aux agents et serveurs de prendre le contenu de cette page en tant que HTML
+    this.metaService.addTag({ name: 'description', content: "Page de paiement du site de location de logement insolite en France" }); // Meta description de la page
+    this.metaService.addTag({ property: 'og-type', content: "Site web"}); // Indique le type de l'objet
+    this.metaService.addTag({ name: 'robots', content: 'noindex, nofollow' });  // Permet au robot d'indexer la page
+    /* this.metaService.addTag({ property: 'og:title', content: 'Content Title for social media' }); */ // Titre pour réseau social
+    /*this.metaService.addTag({ name: 'keywords', content: 'TypeScript, Angular' });*/ //Add keyword
+    /* this.metaService.addTag({ property: 'og:title', content: "My Text" }) */ // Titre pour l'encadré dans les recherches
+  }
+
 
   ngOnInit() {
     this.invokeStripe();
